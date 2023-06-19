149edda207b2458a4a1b32ee7707c2822c8b05be
jma199455 <jma199455@gmail.com>
2023-06-19 17:16:36 +0900

    join api 테스트 에러 처리

--- src/main/java/com/fastcampus/sns/controller/request/UserJoinRequest.java
+++ src/main/java/com/fastcampus/sns/controller/request/UserJoinRequest.java
@@ -13,6 +13,7 @@ public class UserJoinRequest {
     private String userName;
     private String password;
 
+    // ObjectMapper가 @RequestBody를 바인딩할 때 기본 생성자를 사용하기 때문에 기본생성자를 만들어 줬어야 한다.
     public UserJoinRequest() {
 
     }
