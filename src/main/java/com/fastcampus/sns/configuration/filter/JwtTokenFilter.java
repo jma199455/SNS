89e2d54ea631630c6a3ff7812a1ca72fcbb37693
jma199455 <jma199455@gmail.com>
2023-07-17 17:14:20 +0900

    Filter 오류 발생 부분 수정 후 커밋

--- src/main/java/com/fastcampus/sns/configuration/filter/JwtTokenFilter.java
+++ src/main/java/com/fastcampus/sns/configuration/filter/JwtTokenFilter.java
@@ -27,37 +27,37 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private final String key;
     private final UserService userService;
 
+    private final static List<String> TOKEN_IN_PARAM_URLS = List.of("/api/v1/users/alarm/subscribe");
 
-    // 요청이 들어올 때 filter 설정
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // get header
-        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if(header == null || !header.startsWith("Bearer")) {
-            log.error("Error occurs while getting header. header is null or invalid");
-            filterChain.doFilter(request,response);
-            return;
-        }
-
+        final String token;
         try {
-            final String token = header.split(" ")[1].trim();
+            if (TOKEN_IN_PARAM_URLS.contains(request.getRequestURI())) {
+                log.info("Request with {} check the query param", request.getRequestURI());
+                token = request.getQueryString().split("=")[1].trim();
+            } else {
+                final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+                if (header == null || !header.startsWith("Bearer ")) {
+                    log.error("Error occurs while getting header. header is null or invalid {}", request.getRequestURL());
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+                token = header.split(" ")[1].trim();
+            }
 
-            // check token is valid
             if (JwtTokenUtils.isExpired(token, key)) {
                 log.error("Key is expired");
                 filterChain.doFilter(request, response);
                 return;
             }
 
-            // get username from token
             String userName = JwtTokenUtils.getUserName(token, key);
-            // check the user is valid
             User user = userService.loadUserByUserName(userName);
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                     user, null, user.getAuthorities());
-
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (RuntimeException e) {
@@ -66,14 +66,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             return;
         }
 
-        filterChain.doFilter(request,response);
-
-
-
+        filterChain.doFilter(request, response);
     }
-
-
-
-
-
 }
