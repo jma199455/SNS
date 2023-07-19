8678db9abcd98debc193dcf878a3b7fa2c0e28bc
jma199455 <jma199455@gmail.com>
2023-07-19 15:10:49 +0900

    JPA delete사용하지 않고 delete 쿼리 메소드 추가

--- src/main/java/com/fastcampus/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/fastcampus/sns/repository/CommentEntityRepository.java
@@ -7,9 +7,11 @@ import com.fastcampus.sns.model.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +21,12 @@ public interface CommentEntityRepository extends JpaRepository<CommentEntity,Int
 
 
     Page<CommentEntity> findAllByPost(PostEntity post, Pageable page);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE CommentEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
+
+
 
 }
